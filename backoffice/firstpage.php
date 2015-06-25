1fe7cd93c7b9c58b1a674dd605c20a03c5b5a86b
sdcsdc <sdcsdcsdc>
2015-06-25 10:05:28 +0200

    DATATABLE

--- backoffice/firstpage.php
+++ backoffice/firstpage.php
@@ -13,11 +13,32 @@ $all_enregistrement = getAllEnregistrement($dbh, $_SESSION['id']);
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="../datatable/media/css/jquery.dataTables.css">
+	<style type="text/css" class="init">
 
-    <link rel="stylesheet" href="css/styles.css" />
+	</style>
+	<script type="text/javascript" language="javascript" src="../datatable/media/js/jquery.js"></script>
+	<script type="text/javascript" language="javascript" src="../datatable/media/js/jquery.dataTables.js"></script>
+	<script type="text/javascript" language="javascript" class="init">
+	$(document).ready(function() {
+    	$('#enregistrement').DataTable();
+	} );
+</script>
   </head>
-  <body >
 
+
+  <body>
+  	<div class="container">
+  	<table id="enregistrement" class="display" cellspacing="0" width="100%">
+  		<thead>
+  			<tr>
+  				<th>id</th>
+  				<th>title</th>
+  				<th>created</th>
+  				<th>actions</th>
+  			</tr>
+  		</thead>
+  	</table>
+  </div>
   </body>
-
 
