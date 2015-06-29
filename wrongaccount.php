c4b83a2b50cb8d8caec7d7cd5308d85c3111d7eb
Vanessa <hecatesias@gmail.com>
2015-06-29 10:20:55 +0200

    Update wrongaccount.php

--- wrongaccount.php
+++ wrongaccount.php
@@ -2,3 +2,14 @@
 
 //Si mec plante de mdp
 //Nouveau form plus reset
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=esticontroller', $user, $password);
+    foreach($dbh->query('SELECT * from estiuser') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    //die();
+}
